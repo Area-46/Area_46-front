7bcfaa4af192c6362e256478138313a0bf76f170
Natthapong_Pop <popbobkok34@hotmail.com>
2022-03-14 16:23:23 +0700

    update deploy

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.post('/addMenu', (req, res) => {
 });
 
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.listen(8080, function() {
+    console.log("Server started on port 8080");
 });
